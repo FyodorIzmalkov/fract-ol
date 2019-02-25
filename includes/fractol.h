88c8eb218c4649c8f262fd9687c4e255d9cc895a
Lamprey Sandor frey <fedorizmalkov@gmail.com>
2019-02-25 21:42:03 +0300

    changes

---
+++ includes/fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsandor- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/06 18:42:45 by lsandor-          #+#    #+#             */
+/*   Updated: 2019/02/25 21:41:48 by lsandor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdlib.h>
+# include <math.h>
+# include <errno.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include "libft.h"
+# define W_WIDTH 2000
+# define W_HEIGHT 1200
+
+#endif
